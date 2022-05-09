43d335f2f200fdf53a8246708944eb8afc59c1eb
Nirix <64707234+Nirix121@users.noreply.github.com>
2022-05-10 00:41:44 +0300

    Update OBD.py

--- OBD/OBD.py
+++ OBD/OBD.py
@@ -15,8 +15,23 @@ print ('''
 ░░░░░░░░██░░██░░██████░░█████░░░░░░░░░░░░░░░
 ░░░░░░░░██░░██░░██░░░█░░░░░░█░░░░░░░░░░░░░░░
 ░░░░░░░░██████░░█████░░░█████░░░░░░░░░░░░░░░
+ORIGINAL. BASIC. DDOS
+ #OBD help
+   -h/help  | showing this message
+   -url     | set target url
+   -m/mode  | set program mode
+   -data    | set post data path (only works on post mode)
+            | (Example: -data data.json)
+   -cookies | set cookies (Example: 'id:xxx;ua:xxx')
+   -v       | set proxy type (4/5/http, default:5)
+   -t       | set threads number (default:800)
+   -f       | set proxies file (default:proxy.txt)
+   -b       | enable/disable brute mode
+            | Enable=1 Disable=0  (default:0)
+   -s       | set attack time(default:60)
+   -down    | download proxies
+   -check   | check proxies
 
-Original . Basic . Ddos
  ''')
 acceptall = [
 		"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
